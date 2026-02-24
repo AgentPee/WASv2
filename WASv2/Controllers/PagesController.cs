1db9937653c32b5f271de0e1a167d20b2f6c7c23
ITAdmin <ITAdmin@TRAININGCOMPLAB>
2026-02-24 14:23:18 +0800

    Add project files.

---
+++ WASv2/Controllers/PagesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WASv2.Models;
+
+namespace WASv2.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PurchaseRequest()
+        {
+            return View();
+        }
+
+        public IActionResult RFQModule()
+        {
+            return View();
+        }
+
+        public IActionResult PurchaseOrder()
+        {
+            return View();
+        }
+        
+        public IActionResult Accreditation()
+        {
+            return View();
+        }
+
+        public IActionResult Delivery()
+        {
+            return View();
+        }
+
+        public IActionResult PaymentMemo()
+        {
+            return View();
+        }
+
+        public IActionResult CertificateAcceptance()
+        {
+            return View();
+        }
+
+        public IActionResult CreatePR()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy() 
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
